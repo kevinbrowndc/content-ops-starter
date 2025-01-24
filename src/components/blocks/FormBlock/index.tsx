f41c985b4bfeacb956927ae829bdf9ca1f593a44
Kevin Brown <kevin@hrinnovate.org>
2025-01-24 15:11:23 +0200

    Update index.tsx

--- src/components/blocks/FormBlock/index.tsx
+++ src/components/blocks/FormBlock/index.tsx
@@ -22,7 +22,7 @@ export default function FormBlock(props) {
     }
 
     return (
-        <form data-netlify=true>
+        <form>
             
             className={classNames(
                 'sb-component',
