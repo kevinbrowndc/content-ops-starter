73d28a75f20d6cfdaac92799ebb638bc893d763d
Kevin Brown <kevin@hrinnovate.org>
2025-01-26 14:38:35 +0200

    Update index.tsx

--- src/components/blocks/FormBlock/index.tsx
+++ src/components/blocks/FormBlock/index.tsx
@@ -22,7 +22,7 @@ export default function FormBlock(props) {
     }
 
     return (
-        <form 
+        <form enctype=“application/x-www-form-urlencoded"
             className={classNames(
                 'sb-component',
                 'sb-component-block',
