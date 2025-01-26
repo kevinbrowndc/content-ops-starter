260a3791627784e52b7fe7fdf3a508581022a038
Kevin Brown <kevin@hrinnovate.org>
2025-01-26 09:08:30 +0200

    Update index.tsx

--- src/components/blocks/FormBlock/index.tsx
+++ src/components/blocks/FormBlock/index.tsx
@@ -22,7 +22,7 @@ export default function FormBlock(props) {
     }
 
     return (
-        <form onSubmit={handleFormSubmit}
+        <form onSubmit={handleSubmit}
             className={classNames(
                 'sb-component',
                 'sb-component-block',
