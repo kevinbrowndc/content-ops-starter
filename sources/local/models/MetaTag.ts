03029acd3db40ab4f940b6949125c290d7529bbf
Kevin Brown <kevin@hrinnovate.org>
2025-01-27 20:33:39 +0200

    Update MetaTag.ts

--- sources/local/models/MetaTag.ts
+++ sources/local/models/MetaTag.ts
@@ -5,19 +5,7 @@ export const metadata = {
     description: 'The React Framework for the Web',
     url: 'https://nextjs.org',
     siteName: 'Next.js',
-    images: [
-      {
-        url: 'https://nextjs.org/og.png',
-        width: 800,
-        height: 600,
-      },
-      {
-        url: 'https://nextjs.org/og-alt.png',
-        width: 1800,
-        height: 1600,
-        alt: 'My custom alt',
-      },
-    ],
+
 export const MetaTag: Model = {
   type: 'object',
   name: 'MetaTag',
