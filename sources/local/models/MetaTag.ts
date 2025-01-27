bcdee782552a4f1887d4105157d0050367cd2abc
Kevin Brown <kevin@hrinnovate.org>
2025-01-27 20:26:20 +0200

    Update MetaTag.ts

--- sources/local/models/MetaTag.ts
+++ sources/local/models/MetaTag.ts
@@ -1,5 +1,23 @@
 import { Model } from '@stackbit/types';
-
+export const metadata = {
+  openGraph: {
+    title: 'Next.js',
+    description: 'The React Framework for the Web',
+    url: 'https://nextjs.org',
+    siteName: 'Next.js',
+    images: [
+      {
+        url: 'https://nextjs.org/og.png',
+        width: 800,
+        height: 600,
+      },
+      {
+        url: 'https://nextjs.org/og-alt.png',
+        width: 1800,
+        height: 1600,
+        alt: 'My custom alt',
+      },
+    ],
 export const MetaTag: Model = {
   type: 'object',
   name: 'MetaTag',
